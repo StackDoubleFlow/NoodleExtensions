84b8a4aa9d1ec0d55b930c8542f3f8f445691f49
StackDoubleFlow <ojaslandge@gmail.com>
2021-07-16 23:21:56 -0500

    Fix qmod build

--- scripts/buildQMOD.sh
+++ scripts/buildQMOD.sh
@@ -6,8 +6,8 @@ buildScript="$NDKPath/build/ndk-build"
 
 ./$buildScript NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk
 mkdir BMBFBuild
-cp "./bmbfmod.json" "./libs/arm64-v8a/libnoodleextensions.so" ./BMBFBuild
+cp "./mod.json" "./libs/arm64-v8a/libnoodleextensions.so" ./BMBFBuild
 cd BMBFBuild
-zip "../NoodleExtensions.zip" *
+zip "../NoodleExtensions.qmod" *
 cd ..
 rm -r BMBFBuild
