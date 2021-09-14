d71861c670ef140f765fb0e0ff4f424f7ec340f1
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-09-13 20:08:49 -0400

    Rotate rotated object and fix condition (my bad)

--- src/Hooks/NoteFloorMovement.cpp
+++ src/Hooks/NoteFloorMovement.cpp
@@ -46,8 +46,8 @@ MAKE_HOOK_MATCH(NoteFloorMovement_ManualUpdate, &NoteFloorMovement::ManualUpdate
 MAKE_HOOK_MATCH(NoteFloorMovement_SetToStart, &NoteFloorMovement::SetToStart, UnityEngine::Vector3, NoteFloorMovement *self) {
     auto ret = NoteFloorMovement_SetToStart(self);
 
-    if (noteUpdateAD && noteUpdateAD->objectData.disableNoteLook) {
-        self->get_transform()->set_localRotation(NEVector::Quaternion::Euler({0, 0, noteUpdateAD->endRotation}));
+    if (noteUpdateAD && noteUpdateAD->objectData.disableNoteLook.has_value() && noteUpdateAD->objectData.disableNoteLook.value()) {
+        self->rotatedObject->set_localRotation(NEVector::Quaternion::Euler({0, 0, noteUpdateAD->endRotation}));
     }
 
     return ret;
