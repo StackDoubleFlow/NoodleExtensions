cdd178d626329d4ce91269d14fb9f2d700d15067
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-12-18 17:12:27 -0400

    Fix beatmap object count

--- include/SceneTransitionHelper.hpp
+++ include/SceneTransitionHelper.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "custom-json-data/shared/JSONWrapper.h"
+#include "NELogger.h"
 
 namespace GlobalNamespace {
     class IDifficultyBeatmap;
@@ -13,8 +14,34 @@ namespace CustomJSONData {
 namespace NoodleExtensions {
     class SceneTransitionHelper {
     public:
-        static bool CheckIfInArray(ValueUTF16 const& val, std::u16string_view stringToCheck);
+        static bool CheckIfInArray(ValueUTF16 const& val, std::u16string_view stringToCheck) {
+            if (val.IsArray()) {
+                for (auto const& element : val.GetArray()) {
+                    if (element.IsString() && element.GetString() == stringToCheck)
+                        return true;
+                }
+            }
+
+            if (val.IsObject()) {
+                for (auto const& element : val.GetObject()) {
+                    if (element.value.IsString() && element.value.GetString() == stringToCheck)
+                        return true;
+                }
+            }
+
+            return false;
+        }
 
         static void Patch(GlobalNamespace::IDifficultyBeatmap* difficultyBeatmap, CustomJSONData::CustomBeatmapData * customBeatmapDataCustom);
+
+        static bool CheckIfNoodle(ValueUTF16 const& rapidjsonData) {
+            auto requirements = rapidjsonData.FindMember(u"_requirements");
+
+            if (requirements != rapidjsonData.MemberEnd()) {
+                return CheckIfInArray(requirements->value, REQUIREMENTNAME);
+            }
+
+            return false;
+        }
     };
 }
---
+++ src/Hooks/FakeNotes/BeatmapData.cpp
@@ -0,0 +1,128 @@
+#include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
+#include "beatsaber-hook/shared/utils/hooking.hpp"
+
+#include "GlobalNamespace/BeatmapData.hpp"
+#include "GlobalNamespace/BeatmapDataLoader.hpp"
+#include "GlobalNamespace/BeatmapSaveData.hpp"
+#include "GlobalNamespace/BeatmapSaveData_NoteData.hpp"
+#include "GlobalNamespace/BeatmapSaveData_ObstacleData.hpp"
+#include "GlobalNamespace/BeatmapSaveData_WaypointData.hpp"
+#include "GlobalNamespace/BeatmapSaveData_EventData.hpp"
+#include "GlobalNamespace/BeatmapSaveData_SpecialEventKeywordFiltersData.hpp"
+#include "GlobalNamespace/BeatmapSaveData_SpecialEventsForKeyword.hpp"
+
+#include "custom-json-data/shared/CustomBeatmapData.h"
+#include "pinkcore/shared/API.hpp"
+
+#include "FakeNoteHelper.h"
+#include "SceneTransitionHelper.hpp"
+#include "NEHooks.h"
+#include "NEJSON.h"
+
+#include "songloader/shared/API.hpp"
+
+
+typedef rapidjson::GenericDocument<rapidjson::UTF16<char16_t>> DocumentUTF16;
+
+using namespace System;
+using namespace System::Collections::Generic;
+using namespace GlobalNamespace;
+using namespace CustomJSONData;
+
+struct BeatmapRemoveData {
+    int toRemoveObstacle = 0;
+    int toRemoveNote = 0;
+    int toRemoveBomb = 0;
+};
+
+static std::unordered_map<BeatmapData*, BeatmapRemoveData> beatmapRemoveDatas;
+
+// return true if fake
+// subtracts from object count if fake
+static bool FakeObjectRemove(BeatmapData *self, BeatmapObjectData* beatmapObjectData, BeatmapRemoveData& beatmapRemoveData)
+{
+    static auto *customObstacleDataClass = classof(CustomJSONData::CustomObstacleData *);
+    static auto *customNoteDataClass = classof(CustomJSONData::CustomNoteData *);
+
+    int* countField = nullptr;
+    CustomJSONData::JSONWrapper *customDataWrapper = nullptr;
+
+    if (beatmapObjectData->klass == customObstacleDataClass) {
+        auto obstacleData =
+                (CustomJSONData::CustomObstacleData *)beatmapObjectData;
+        customDataWrapper = obstacleData->customData;
+        countField = &beatmapRemoveData.toRemoveObstacle;
+    } else if (beatmapObjectData->klass == customNoteDataClass) {
+        auto noteData = (CustomJSONData::CustomNoteData *)beatmapObjectData;
+        customDataWrapper = noteData->customData;
+
+        if (noteData->cutDirection == NoteCutDirection::None)
+        {
+            // bomb
+            countField = &beatmapRemoveData.toRemoveBomb;
+        } else {
+            countField = &beatmapRemoveData.toRemoveNote;
+        }
+    }
+
+    if (customDataWrapper && customDataWrapper->value) {
+        rapidjson::Value const& customData = *customDataWrapper->value;
+
+        auto fake = NEJSON::ReadOptionalBool(customData, "_fake");
+
+        if (fake.value_or(false)) {
+            int& countRef = *countField;
+            countRef++;
+
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+static void OnBeatmapLoad(GlobalNamespace::StandardLevelInfoSaveData* infoDat, const std::string& filepath, GlobalNamespace::BeatmapData* beatmapData) {
+    auto it = beatmapRemoveDatas.find(beatmapData);
+
+    if (it != beatmapRemoveDatas.end()) {
+        auto const &data = it->second;
+
+        bool isNoodle = false;
+        if (auto customBeatmap = il2cpp_utils::try_cast<CustomBeatmapData>(beatmapData)) {
+            if (customBeatmap.value()->levelCustomData) {
+                auto customData = customBeatmap.value()->levelCustomData->value;
+
+                if (customData) {
+                    isNoodle = NoodleExtensions::SceneTransitionHelper::CheckIfNoodle(*customData);
+                }
+            }
+        }
+
+
+        if (isNoodle) {
+            beatmapData->bombsCount -= data.toRemoveBomb;
+            beatmapData->cuttableNotesCount -= data.toRemoveNote;
+            beatmapData->obstaclesCount -= data.toRemoveObstacle;
+        }
+
+        beatmapRemoveDatas.erase(it);
+    }
+}
+
+MAKE_HOOK_MATCH(BeatmapData_AddBeatmapObjectData_t, &BeatmapData::AddBeatmapObjectData, void,
+               BeatmapData *self, BeatmapObjectData* beatmapObjectData) {
+    BeatmapData_AddBeatmapObjectData_t(self, beatmapObjectData);
+
+    FakeObjectRemove(self, beatmapObjectData, beatmapRemoveDatas[self]);
+}
+
+void InstallBeatmapDataHooks(Logger &logger) {
+    INSTALL_HOOK(logger, BeatmapData_AddBeatmapObjectData_t);
+
+    // force CJD to be first
+    Modloader::requireMod("CustomJSONData");
+    RuntimeSongLoader::API::AddBeatmapDataLoadedEvent(&OnBeatmapLoad);
+
+}
+NEInstallHooks(InstallBeatmapDataHooks);
