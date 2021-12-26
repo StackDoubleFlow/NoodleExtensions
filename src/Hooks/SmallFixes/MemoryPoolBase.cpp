eb70555dd2221e7d77b3efef8963d2e6f84d09fb
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-12-25 20:27:36 -0400

    Remove future proofing, more optimizing!

--- src/Hooks/SmallFixes/MemoryPoolBase.cpp
+++ src/Hooks/SmallFixes/MemoryPoolBase.cpp
@@ -76,9 +76,11 @@ struct ::il2cpp_utils::il2cpp_type_check::MetadataGetter<&Zenject::MemoryPoolBas
 
 // I love generics and il2cpp
 MAKE_HOOK(MemoryPoolBase_Despawn, nullptr, void, MemoryPoolBase_1<Il2CppObject*> *self, Il2CppObject* item, MethodInfo* methodInfo) {
-    static auto ObstacleControllerKlass = classof(ObstacleController*);
+    static auto* ObstacleControllerKlass = classof(ObstacleController*);
 
-    if (!item || !il2cpp_functions::class_is_assignable_from(ObstacleControllerKlass, item->klass)) {
+    if (!item || (item->klass != ObstacleControllerKlass
+    /* && !il2cpp_functions::class_is_assignable_from(ObstacleControllerKlass, item->klass) */ // TODO: Is this future proofing even needed?
+    )) {
         MemoryPoolBase_Despawn(self, item, methodInfo);
         return;
     }
