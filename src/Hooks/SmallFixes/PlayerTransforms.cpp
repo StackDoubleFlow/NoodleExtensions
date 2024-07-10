40fce614059e64606049ec42cd6420d90814110a
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2024-07-09 21:01:00 -0400

    Fix player transform headset offset z

--- src/Hooks/SmallFixes/PlayerTransforms.cpp
+++ src/Hooks/SmallFixes/PlayerTransforms.cpp
@@ -7,18 +7,28 @@
 
 #include "NEHooks.h"
 
+#include "tracks/shared/Vector.h"
+
 using namespace GlobalNamespace;
 using namespace UnityEngine;
 
-MAKE_HOOK_MATCH(PlayerTransforms_Update, &PlayerTransforms::Update, void, PlayerTransforms* self) {
-  if (!Hooks::isNoodleHookEnabled()) return PlayerTransforms_Update(self);
+MAKE_HOOK_MATCH(PlayerTransforms_Awake, &PlayerTransforms::Awake, void, PlayerTransforms* self) {
+  if (!Hooks::isNoodleHookEnabled()) return PlayerTransforms_Awake(self);
 
-  PlayerTransforms_Update(self);
-  self->_headPseudoLocalPos = self->_headTransform->get_localPosition();
+  PlayerTransforms_Awake(self);
+  self->_useOriginParentTransformForPseudoLocalCalculations = false;
+}
+MAKE_HOOK_MATCH(PlayerTransforms_HeadsetOffsetZ, &PlayerTransforms::HeadOffsetZ, float, PlayerTransforms* self,
+                ::UnityEngine::Quaternion noteInverseWorldRotation) {
+  if (!Hooks::isNoodleHookEnabled()) return PlayerTransforms_HeadsetOffsetZ(self, noteInverseWorldRotation);
+
+  // get magnitude in direction we care about rather than just z
+  return NEVector::Vector3::Dot(NEVector::Quaternion(noteInverseWorldRotation) * self->_headPseudoLocalPos, self->_originParentTransform->forward);
 }
 
 void InstallPlayerTransformsHooks() {
-  INSTALL_HOOK(NELogger::Logger, PlayerTransforms_Update);
+  INSTALL_HOOK(NELogger::Logger, PlayerTransforms_Awake);
+  INSTALL_HOOK(NELogger::Logger, PlayerTransforms_HeadsetOffsetZ);
 }
 
 NEInstallHooks(InstallPlayerTransformsHooks);
